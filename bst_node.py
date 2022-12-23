916c8c0c971ac588f850bf94165494ffab1cbafc
boulas627 <43757451+boulas627@users.noreply.github.com>
2022-12-23 15:28:19 -0500

    Delete bst_node.py

--- bst_node.py
+++
@@ -1,6 +0,0 @@
-class Node: 
-	def __init__(self, question, correct_answer, difficulty): 
-		self.question = question 
-		self.correct_answer = correct_answer
-		self.left = None
-		self.right = None
